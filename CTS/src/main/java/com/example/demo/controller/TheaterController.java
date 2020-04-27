72f70d9a323a0e671c9861d44f58760ba31f940d
Shivangi Sharma <Shivangi Sharma@192.168.1.9>
2020-04-28 01:09:25 +0530

    commit

--- CTS/src/main/java/com/example/demo/controller/TheaterController.java
+++ CTS/src/main/java/com/example/demo/controller/TheaterController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.entity.Theater;
 import com.example.demo.repository.CityRepository;
 import com.example.demo.repository.MovieRepository;
 import com.example.demo.repository.ShowTimeRepository;
@@ -28,4 +29,43 @@ public class TheaterController
     
     @Autowired
     private ShowTimeRepository showTimeRepository;
+    
+
+    @GetMapping("/city/{cityId}/theater")
+    public Page<Theater> getAllTheatersByCityId(@PathVariable (value = "cityId") Integer cityId,
+                                                Pageable pageable) {
+        return theaterRepository.findByCityId(cityId, pageable);
+    }
+
+    @PostMapping("/city/{cityId}/theater")
+    public Theater createTheater(@PathVariable (value = "cityId") Integer cityId,
+                                 @Valid @RequestBody Theater theater) {
+        return cityRepository.findById(cityId).map(city -> {
+            theater.setCity(city);
+            return theaterRepository.save(theater);
+        }).orElseThrow(() -> new ResourceNotFoundException("cityId " + cityId + " not found"));
+    }
+
+    @PutMapping("/city/{cityId}/theater/{theaterId}")
+    public Theater updateTheater(@PathVariable (value = "cityId") Integer cityId,
+                                 @PathVariable (value = "theaterId") Integer theaterId,
+                                 @Valid @RequestBody Theater theaterRequest) {
+        if(!cityRepository.existsById(cityId)) {
+            throw new ResourceNotFoundException("CityId " + cityId + " not found");
+        }
+
+        return theaterRepository.findById(theaterId).map(theater -> {
+            theater.settName(theaterRequest.gettName());
+            return theaterRepository.save(theater);
+        }).orElseThrow(() -> new ResourceNotFoundException("TheaterId " + theaterId + "not found"));
+    }
+
+    @DeleteMapping("/city/{cityId}/theater/{theaterId}")
+    public ResponseEntity<?> deleteTheater(@PathVariable (value = "cityId") Integer cityId,
+                              @PathVariable (value = "theaterId") Integer theaterId) {
+        return theaterRepository.findByIdAndCityId(theaterId, cityId).map(theater -> {
+            theaterRepository.delete(theater);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Theater not found with id " + theaterId + " and CityId " + cityId));
+    }
 }
--- CTS/src/main/java/com/example/demo/repository/TheaterRepository.java
+++ CTS/src/main/java/com/example/demo/repository/TheaterRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Theater;
 import org.springframework.stereotype.Repository;
@@ -7,5 +9,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TheaterRepository extends JpaRepository<Theater, Integer>
 {
+
+	Page<Theater> findByCityId(Integer cityId, Pageable pageable);
+
+	Page<Theater> findByIdAndCityId(Integer theaterId, Integer cityId);
 
 }
